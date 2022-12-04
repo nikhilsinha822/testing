811f45aeb5e50782b09211cb7d873213251ad7db
nikhilsinha <nikhilsinha822@gmail.com>
2022-12-04 20:29:39 +0530

    cart and prd ejs

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const bodyparser = require("body-parser")
+
+const app = express();
+
+app.set('view engine','ejs');
+app.use(bodyparser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+app.get("/",(req,res)=>{
+    res.render("home");
+})
+app.get("/cart",(req,res)=>{
+    res.render("cart");
+})
+app.get("/product",(req,res)=>{
+    res.render("product");
+})
+
+app.listen(3000,()=>{
+    console.log("Server in running");
+})
